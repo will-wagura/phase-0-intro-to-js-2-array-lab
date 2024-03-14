dc1a3af4f5490ff17bbe8eff61ff489c499bd84c
William Ndirangu <wwagura0@gmail.com>
2024-03-15 00:18:17 +0300

    Add

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(){
+    return [...cats, "Broom"];
+}
+function prependCat(){
+    return ["Arnold", ...cats];
+}
+function removeLastCat(){
+    return cats.slice(0, -1);
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
